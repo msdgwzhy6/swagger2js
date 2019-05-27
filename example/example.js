9295b410878b793aad3755e09c6792b184129511
yin <yin@swifthealth.cn>
2019-05-27 19:08:32 +0800

    base

--- example/example.js
+++ example/example.js
@@ -8,6 +8,6 @@
 const {swagger2js} = require('../index');
 
 swagger2js({
-  swaggerUrl: 'http://192.168.1.51:10101/doctor/v2/api-docs',
+  swaggerUrl: 'http://192.168.1.16:8080/manage/v1/api-docs',
   pathName: __dirname + '/API'
 });
